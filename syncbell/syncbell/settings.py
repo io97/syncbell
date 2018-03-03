be61d99d702f4d0e1487677d6de7747cb63d9b64
io97 <io97cux@gmail.com>
2018-03-03 21:51:33 +0900

    settingsにmoneyflyアプリケーションを追加

--- syncbell/syncbell/settings.py
+++ syncbell/syncbell/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'moneyfly.apps.MoneyflyConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
