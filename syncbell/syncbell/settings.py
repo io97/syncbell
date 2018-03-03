db16d49b743c8b594a99786099dabddd613e646f
io97 <io97cux@gmail.com>
2018-03-03 22:43:34 +0900

    カンマつけ忘れ修正

--- syncbell/syncbell/settings.py
+++ syncbell/syncbell/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'moneyfly.apps.MoneyflyConfig'
+    'moneyfly.apps.MoneyflyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
