b7b0a566a95ea9506a724d6ac8ba06289c574418
io97 <io97cux@gmail.com>
2018-03-03 22:38:57 +0900

    modelsの設定

--- syncbell/moneyfly/models.py
+++ syncbell/moneyfly/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+""" カテゴリ """
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+""" 金額 """
+class Money(models.Model):
+    money = models.IntegerField()
+    category = models.ForeignKey(
+        Category,
+        on_delete = models.CASCADE,
+    )
+    note = models.TextField()
+    use_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.category_id
