59e0214c7ff187f9ab3dd6509e2e4fc94d8e37ca
io97 <io97cux@gmail.com>
2018-03-04 23:05:31 +0900

    当月の合計金額を表示

--- syncbell/moneyfly/views.py
+++ syncbell/moneyfly/views.py
@@ -1,13 +1,29 @@
 from django.views import generic
 from django.urls import reverse_lazy
 from .models import Money
+from django.db.models import Sum
+
+from datetime import date
 
 
 class IndexView(generic.ListView):
     model = Money
-    paginate_by = 8
+    paginate_by = 5
     ordering = ['-use_at']
     template_name = "moneyfly/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # 当月の合計金額
+        this_y = date.today().year
+        this_m = date.today().month
+        context["month"] = this_m
+        context["sum"] = Money.objects\
+                         .filter(use_at__year = this_y,
+                                 use_at__month = this_m)\
+                         .aggregate(Sum('money'))["money__sum"]
+        return context
 
 
 class DetailView(generic.DetailView):
