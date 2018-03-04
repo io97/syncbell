09873ae55072f0b0cba18a746515ed13264ffe4d
io97 <io97cux@gmail.com>
2018-03-04 23:08:07 +0900

    パスの変更

--- syncbell/moneyfly/urls.py
+++ syncbell/moneyfly/urls.py
@@ -5,8 +5,8 @@ app_name = "moneyfly"
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
-    path("money/create/", views.CreateView.as_view(), name="create"),
-    path("money/<int:pk>/", views.DetailView.as_view(), name="detail"),
-    path("money/<int:pk>/update/", views.UpdateView.as_view(), name="update"),
-    path("money/<int:pk>/delete/", views.DeleteView.as_view(), name="delete"),
+    path("create/", views.CreateView.as_view(), name="create"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/update/", views.UpdateView.as_view(), name="update"),
+    path("<int:pk>/delete/", views.DeleteView.as_view(), name="delete"),
 ]
